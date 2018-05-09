16c38e8f865b52a2811ba8fb52ae747f51b37a65
kamran haider <kamranhaider.mb@gmail.com>
2018-05-09 14:20:36 -0400

    recent changes

--- pymcce/mcce_simulation.py
+++ pymcce/mcce_simulation.py
@@ -1,8 +1,8 @@
-import os
+import os, sys
 import struct
 import numpy as np
 import mdtraj as md
-
+from io import StringIO
 
 HEAD3_HEADER = {'FL': 0, 'occ': 1, 'crg': 2, 'Em0': 3, 'pKa0': 4, 'ne': 5,
                 'nH': 6, 'vdw0': 7, 'vdw1': 8, 'tors': 9, 'epol': 10,
@@ -17,7 +17,10 @@ class Simulation(object):
         self.ms_data_file = ms_data_file
         self.byte_indices = None
         self.total_microstates = 0
+
         res_list = []
+
+
 
         with open(self.ms_data_file, "rb") as md:
             bytes_n_res = md.read(4)
@@ -31,18 +34,49 @@ class Simulation(object):
 
         # with open(ms_gold_file, "r") as ms_gold:
         #    self.n_res = len([res.strip() for res in ms_gold.readlines() if len(res) != 0])
-        conf_data = {}
-        conf_id_name_map = {}
+
+        self.conf_id_name_map = {}
+        self.iConf = {}
+        self.FL = {}
+        self.crg = {}
+        self.Em0 = {}
+        self.pKa0 = {}
+        self.ne = {}
+        self.nH = {}
+        self.vdw0 = {}
+        self.vdw1 = {}
+        self.tors = {}
+        self.epol = {}
+        self.dsolv = {}
+        self.extra = {}
+
+        data_holders = [self.iConf, self.FL, self.crg, self.Em0,
+                        self.pKa0, self.ne, self.nH, self.vdw0, self.vdw1, self.tors, self.epol,
+                        self.dsolv, self.extra]
+
+
         with open(head3lst_file, "r") as h3:
             for line in h3.readlines()[1:]:
                 data = line.split()
-                conf_id = data[1]
-                conf_data[conf_id] = [int(data[0]), 0.0, []]
-                conf_id_name_map[int(data[0])] = conf_id
-        self.conformer_data = conf_data
-        self.conf_id_name_map = conf_id_name_map
+                conf_id, conf_name,  = int(data[0]), data[1]
+                self.conf_id_name_map[conf_id] = conf_name
+                self.iConf[conf_name] = conf_id
+                self.FL[conf_name] = data[2]
+                self.crg[conf_name] = float(data[4])
+                self.Em0[conf_name] = float(data[5])
+                self.pKa0[conf_name] = float(data[6])
+                self.ne[conf_name] = float(data[7])
+                self.nH[conf_name] = float(data[8])
+                self.vdw0[conf_name] = float(data[9])
+                self.vdw1[conf_name] = float(data[10])
+                self.tors[conf_name] = float(data[11])
+                self.epol[conf_name] = float(data[12])
+                self.dsolv[conf_name] = float(data[13])
+                self.extra[conf_name] = float(data[14])
+
 
         # read fort38 data
+        conf_occ = {}
         with open(fort38_file, 'r') as f:
             lines = [l.strip().split() for l in f.readlines()]
             header = lines.pop(0)
@@ -50,20 +84,25 @@ class Simulation(object):
                 conf_id = index + 1
                 conf_name = l[0]
                 occ = float(l[1])
-                if conf_name in self.conformer_data.keys():
-                    self.conformer_data[conf_name][1] += occ
+                if conf_name in self.iConf.keys():
+                    #self.iConf[conf_name][1] += occ
+                    conf_occ[conf_name] = occ
+                else:
+                    conf_occ[conf_name] = None
+
+        self.conformer_occupancies = conf_occ
 
         residue_data = {}
-        for k in self.conformer_data.keys():
+
+        for k in sorted(self.iConf.keys()):
             conf_name = k
             res_key = conf_name[:3] + conf_name[5:10]
-            if "CTR" not in res_key and "NTR" not in res_key:
-                if res_key not in residue_data.keys():
-                    residue_data[res_key] = [(k, self.conformer_data[k][0])]
-                else:
-                    residue_data[res_key].append((k, self.conformer_data[k][0]))
-
+            if res_key not in residue_data.keys():
+                residue_data[res_key] = [(k, self.iConf[k])]
+            else:
+                residue_data[res_key].append((k, self.iConf[k]))
         self.residue_data = residue_data
+
 
     def generate_byte_indices(self, sample_frequency, filename):
         """
@@ -118,9 +157,20 @@ class Simulation(object):
                 energies[index] += energy
                 progress_counter += 1
                 # print_progress_bar(progress_counter, self.total_records)
+
         self.trajectory = trajectory
         self.state_counts = state_counts
         self.energies = energies
+
+    def get_step2out_data(self, step2out_pdb):
+
+        with open(step2out_pdb, "r") as f:
+            atom_lines_step2out = [l.split() for l in f.readlines() if "ATOM" in l]
+            conf_names = [l[3] + l[-1][:2] + l[4] for l in atom_lines_step2out]
+            atom_names = [l[2] for l in atom_lines_step2out]
+            charges = [float(l[-2]) for l in atom_lines_step2out]
+            radii = [float(l[-3]) for l in atom_lines_step2out]
+        return conf_names, charges, radii, atom_names
 
     def parse_struct(self, step2out_pdb, n_conf):
         """
@@ -128,55 +178,76 @@ class Simulation(object):
         """
         print("Parsing structure ...")
         st = md.load_pdb(step2out_pdb, frame=0, no_boxchk=True)
-        for index, res in enumerate(st.topology.residues):
-            if res.name == "HOH":
-                k = "%sA%04d" % (res.name, res.resSeq)
-                residue_at_ids = [at.index for at in res.atoms]
-                for c in range(n_conf):
-                    conf_name = "HOH01W%04d_%03d" % (res.resSeq, c + 1)
-                    conf_rows = c * 3, (c * 3) + 3
-                    conf_at_ids = residue_at_ids[conf_rows[0]:conf_rows[1]]
-                    # conf_st = st.atom_slice(conf_at_ids)
-                    self.conformer_data[conf_name][2].extend(conf_at_ids)
-                    # angle = md.compute_angles(st, [[conf_at_ids[1], conf_at_ids[0], conf_at_ids[2]]])
-                    # angle = md.utils.in_units_of(angle, "radians", "degrees")[0]
-                    # if angle <= 104:
-                    # print(conf_name, angle, conf_at_ids)
+        conf_names, charges, radii, atom_names = self.get_step2out_data(step2out_pdb)
+
+        conformer_atom_data = {}
+        for index, conf in enumerate(conf_names):
+            if conf not in conformer_atom_data.keys():
+                conformer_atom_data[conf] = [[index, charges[index], radii[index], atom_names[index]]]
+            else:
+                conformer_atom_data[conf].append([index, charges[index], radii[index], atom_names[index]])
+
         self.structure = st
+        self.conformer_atom_data = conformer_atom_data
         print("Done.")
 
-    def calculate_water_dipoles(self, chg):
+    def calculate_dipoles(self):
 
-        v = np.array([0, 0, 1])
-        for index, c in enumerate(self.conformer_data.keys()):
-            if len(self.conformer_data[c][-1]) != 0:
-                conf_ids = self.conformer_data[c][-1]
-                pos = self.structure.xyz[0, conf_ids, :].T
-                pos *= -10.0
-                dp = pos.dot(chg)
-                # calculate projection
-                proj = np.multiply(dp.dot(v) / v.dot(v), v)
+        """
 
-                scaling_factor = dp.dot(v) / v.dot(v)
-                # print(index, scaling_factor)
-                # snapshot_st = msa.structure.atom_slice(conf_ids)
-                # snapshot_st.save_pdb("%d_wat_conf.pdb" % index)
+        Returns
+        -------
 
-                self.conformer_data[c].append(scaling_factor)
-            else:
-                self.conformer_data[c].append(None)
+        """
+        local_indices = np.array([(a.index, a.residue.atom(0).index) for a in self.structure.top.atoms], dtype='int32')
+        local_displacements = md.compute_displacements(self.structure, local_indices, periodic=True)
 
-    def parse_energy_data(self):
+        molecule_indices = np.array([(a.residue.atom(0).index, 0) for a in self.structure.top.atoms], dtype='int32')
+        molecule_displacements = md.compute_displacements(self.structure, molecule_indices, periodic=True)
 
-        energies_dir = "/Users/kamranhaider/Dropbox/ProtonHopping_scratch/data/mcce_water_profiling/5_conf_per_water/energies"
-        opp_files = [f for f in os.listdir(energies_dir) if f.endswith(".opp")]
+        xyz = local_displacements + molecule_displacements
+        conf_dipoles = {}
+        for conf in self.conformer_atom_data:
+            conf_at_indices = np.asarray([l[0] for l in self.conformer_atom_data[conf]])
+            conf_charges = np.asarray([l[1] for l in self.conformer_atom_data[conf]])
+            conf_xyz = xyz[0, conf_at_indices, :]
+            #print(conf, conf_xyz.shape, conf_charges.shape)
+            moments = conf_xyz.transpose(1, 0).dot(conf_charges)
+            moments *= 10.0
+            conf_dipoles[conf] = moments
+
+        return conf_dipoles
+
+    def parse_opp_file(self, opp):
+
+        pass
+        """
+        with open(opp, "r") as f1:
+            all_lines = f1.readlines()
+
+            energy_table = [[float(l[20:29]),
+                             float(l[29:37]),
+                             float(l[37:45]),
+                             float(l[45:53])] for l in all_lines]
+
+            return np.asarray(energy_table)
+        """
+    def parse_energy_data(self, opp_dir):
+
+        energies_dir = opp_dir
+        opp_files = [os.path.join(energies_dir, f) for f in os.listdir(energies_dir) if f.endswith(".opp")]
         opp_files = sorted(opp_files)
-        n = len(self.conformer_data.keys())
-        print(len(self.conformer_data.keys()), len(opp_files))
-        pairwise_energies = np.zeros((n, n))
-        for c in self.conformer_data.keys():
-            # opp = os.path.join(energies_dir, f)
-            print(c, opp_files[0], opp_files[1])
-            break
-            # with open(opp, "r") as o:
-            #    data = o.readlines()
+        n = len(self.iConf.keys())
+        energy_matrix_vdw = np.zeros((n, n), dtype=float)
+        energy_matrix_elec = np.zeros((n, n), dtype=float)
+        conf_id_list = sorted(self.conf_id_name_map.keys())
+        conf_names = [self.conf_id_name_map[c] for c in conf_id_list]
+        for index, f in enumerate(conf_names):
+            if "DM" not in f:
+                opp = os.path.join(energies_dir, f + ".opp")
+                print(opp)
+                a = np.genfromtxt(opp, delimiter=(5, 15, 9, 8, 8, 8))[:, 2:4]
+                energy_matrix_elec[index, :] = a[:, 0]
+                energy_matrix_vdw[index, :] = a[:, 1]
+
+        return energy_matrix_elec, energy_matrix_vdw
